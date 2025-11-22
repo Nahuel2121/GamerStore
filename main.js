56712f3e9ebc4434503b5ddad6fd0f6dc20152e7
Nahuel Cardozo <Cardozonahuel76@gmail.com>
2025-11-22 16:29:43 -0300

    feat: agregada la logica del simulador

--- main.js
+++ main.js
@@ -1,33 +1,89 @@
-// Demo operadores
+// aca van los datos de los juego y plataformas
+const plataformas = ["PC", "PlayStation 5", "Xbox Series"];
+const juegos = [
 
-let edad = 19;
-let peronchos = false;
+    { nombre: "Resident Evil Village", precio: 39.99 },
+    { nombre: "Resident Evil Requiem", precio: 52.99 },
+    { nombre: "FIFA 26", precio: 69.99 },
+    { nombre: "Call of Duty: Black Ops 7", precio: 59.99 },
+    { nombre: "Battlefield 6", precio: 69.99 },
+    { nombre: "Hollow Knight: Silksong", precio: 6.99 },
+];
 
-if (edad >= 18 || peronchos){
-    console.log("Podes entrar pibe");
-} else {
-    console.log("Anda a tu casa pendejo");
+function comprarJuego() {
+    let nombre = prompt("Ingrese su nombre:");
+    if (!nombre) {
+        alert("No ingresaste un nombre. Saliendo del simulador.");
+        return;
+    }
+
+    alert("Bienvenido " + nombre + " a la tienda GamerStore!");
+
+    // seleccion de la plataforma
+    let plataformasTexto = "";
+    for (let i = 0; i < plataformas.length; i++) {
+        plataformasTexto += `${i + 1}. ${plataformas[i]}\n`;
+    }
+
+    let opcionPlataforma = prompt(
+        "Seleccione su plataforma:\n" + plataformasTexto
+    );
+
+    if (opcionPlataforma < 1 || opcionPlataforma > plataformas.length) {
+        alert("Opción no válida. Saliendo...");
+        return;
+    }
+
+    const plataformaElegida = plataformas[opcionPlataforma - 1];
+    alert("Elegiste " + plataformaElegida);
+
+    // un carrito para guardar las selecciones cuando finalice el proceso de compra
+    const carrito = [];
+
+    let seguirComprando = true;
+
+    while (seguirComprando) {
+        let textoJuegos = "Seleccione un juego:\n";
+        for (let i = 0; i < juegos.length; i++) {
+            textoJuegos += `${i + 1}. ${juegos[i].nombre} - $${juegos[i].precio}\n`;
+        }
+
+        let opcionJuego = prompt(textoJuegos);
+
+        if (opcionJuego < 1 || opcionJuego > juegos.length) {
+            alert("Opción no válida.");
+        } else {
+            const juegoSeleccionado = juegos[opcionJuego - 1];
+
+            carrito.push(juegoSeleccionado);
+
+            alert(
+                `Agregaste: ${juegoSeleccionado.nombre}\n` +
+                `Precio: $${juegoSeleccionado.precio} USD`
+            );
+        }
+
+        // confirm para seguir comprando
+        seguirComprando = confirm("¿Desea elegir otro juego?");
+    }
+
+    if (carrito.length === 0) {
+        alert("No seleccionaste ningún juego. ¡Hasta la próxima!");
+        return;
+    }
+
+    let resumen = "Resumen de compra:\n\n";
+    let total = 0;
+
+    for (let i = 0; i < carrito.length; i++) {
+        resumen += `- ${carrito[i].nombre}: $${carrito[i].precio}\n`;
+        total += carrito[i].precio;
+    }
+
+    resumen += `\nTOTAL: $${total.toFixed(2)} USD`;
+
+    alert(resumen);
+    alert("Gracias por su compra, " + nombre + "!");
 }
 
-// AND (&&) = se tienen que cumplir ambas condiciones para que el resultado sea true.
-// OR (||) = se tiene que cumplir al menos una de las condiciones.
-
-// ejemplo usando for
-for (let i = 1; i < 6; i++){
-    console.log('Numero: ' + i);
-}
-
-
-// ejemplo usando while
-let a = 1;
-while (a <= 5){
-    console.log('Numero: ' + a);
-    a++;
-}
-
-// ejemplo do while
-let i = 1;
-do{
-    console.log('Numero: ' +i);
-    i++;
-} while (i < 6);
+comprarJuego();
